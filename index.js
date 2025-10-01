0dfdb4e7cc4808eba4da56a19013a027cd6a586c
sina0810 <sinakhoshnaw2@gmail.com>
2025-10-01 16:00:13 -0400

    finished problem 2

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 //Code your solutions in this file
 // Problem 1
 function fiveToOneHundred(n) {
-    for (let i = 5; i <= n; i++) {
-        console.log(i)
-    }
+  for (let i = 5; i <= n; i++) {
+    console.log(i)
+  }
 }
 fiveToOneHundred(100)
 
 //=================================
 // Problem 2 
+// MultiplyTOThree
+
+function multiplesOfThree(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0) {
+      console.log(i)
+    }
+  }
+}
+multiplesOfThree(100)
