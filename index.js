f96c94722fdc4bcd17b0ccc5727e7e6aaad5d59a
[Abel] <[delgadilloabel1@gmail.com]>
2025-10-01 15:45:13 -0400

    completed problem 1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //Code your solutions in this file
+// Problem 1
+function fiveToOneHundred(n) {
+    for (let i = 5; i <= n; i++) {
+        console.log(i)
+    }
+}
+fiveToOneHundred(100)
+
+//=================================
+// Problem 2 
