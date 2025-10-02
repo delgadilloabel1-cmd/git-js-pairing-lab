0b5a310070618282a5b1b9bac5a55a325ec4507a
[Abel] <[delgadilloabel1@gmail.com]>
2025-10-02 11:55:38 -0400

    finished problem 3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //Code your solutions in this file
 // Problem 1
 function fiveToOneHundred(n) {
-  for (let i = 5; i <= n; i++) {
-    console.log(i)
-  }
+    for (let i = 5; i <= n; i++) {
+        console.log(i)
+    }
 }
 fiveToOneHundred(100)
 
@@ -12,13 +12,28 @@ fiveToOneHundred(100)
 // MultiplyTOThree
 
 function multiplesOfThree(n) {
-  for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0) {
-      console.log(i)
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0) {
+            console.log(i)
+        }
     }
-  }
 }
 multiplesOfThree(100)
+
 //////////////////////////////
-// problem 3
-// multiplesOfThreeOrFive
+// Problem 3
+// MultiplesOfThreeOrFive
+
+function multiplesOfThreeOrFive(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i)
+        }
+    }
+
+}
+multiplesOfThreeOrFive(100);
+
+//===================================
+//Problem 4
+//untilNum
